235dda4f866f4d9239f83806ab3fdf11a6b5bf65
Mest <mestevenemail@gmail.com>
2023-11-12 16:31:08 +0000

    Add project files.

---
+++ CaptainsComms/Mod.cs
@@ -0,0 +1,62 @@
+﻿using PulsarModLoader;
+using PulsarModLoader.CustomGUI;
+using UnityEngine;
+
+namespace CaptainsComms
+{
+    public class Mod : PulsarMod
+    {
+        public override string HarmonyIdentifier() => "Mest.CaptainsComms";
+        public override string Name => "Captains Comms";
+        public override string Author => "Mest";
+        public override string Version => "0.0.0";
+        public override string ShortDescription => "Moves comms screen to an accessible location";
+        internal class Config : ModSettingsMenu
+        {
+            public override string Name() => "Captains Comms Config";
+            public override void Draw()
+            {
+                GUILayout.Label("<b><color=yellow>Rejoining will spawn/despawn screens</color></b>");
+                GUILayout.BeginHorizontal();
+                Intrepid.Value = GUILayout.Toggle(Intrepid.Value, "Intrepid Comms Screen");
+                Roland.Value = GUILayout.Toggle(Roland.Value, "Roland Comms Screen");
+                Outrider.Value = GUILayout.Toggle(Outrider.Value, "Outrider Comms Screen");
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                Cruiser.Value = GUILayout.Toggle(Cruiser.Value, "Cruiser Comms Screen");
+                Destroyer.Value = GUILayout.Toggle(Destroyer.Value, "Destroyer Comms Screen");
+                Carrier.Value = GUILayout.Toggle(Carrier.Value, "Carrier Comms Screen");
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                Swordship.Value = GUILayout.Toggle(Swordship.Value, "Swordship Comms Screen");
+                FluffyOne.Value = GUILayout.Toggle(FluffyOne.Value, "FluffyOne Comms Screen");
+                GUILayout.EndHorizontal();
+            }
+            public static SaveValue<bool> Intrepid = new SaveValue<bool>("Intrepid", true);
+            public static SaveValue<bool> Roland = new SaveValue<bool>("Roland", true);
+            public static SaveValue<bool> Outrider = new SaveValue<bool>("Outrider", true);
+            public static SaveValue<bool> Cruiser = new SaveValue<bool>("Cruiser", true);
+            public static SaveValue<bool> Destroyer = new SaveValue<bool>("Destroyer", true);
+            public static SaveValue<bool> Carrier = new SaveValue<bool>("Carrier", true);
+            public static SaveValue<bool> Swordship = new SaveValue<bool>("Swordship", true);
+            public static SaveValue<bool> FluffyOne = new SaveValue<bool>("FluffyOne", true);
+        }
+    }
+    public class UsefulMethods
+    {
+        public static GameObject DuplicateCommsScreen(PLShipInfo __instance)
+        {
+            foreach (PLCommsScreen pLCommsScreen in __instance.InteriorDynamic.GetComponentsInChildren<PLCommsScreen>())
+            {
+                if (pLCommsScreen == null) continue;
+                GameObject NewScreen = GameObject.Instantiate(pLCommsScreen.gameObject);
+                NewScreen.transform.parent = pLCommsScreen.transform.parent;
+                NewScreen.layer = pLCommsScreen.gameObject.layer;
+                NewScreen.transform.position = __instance.CaptainsChairPivot.transform.position;
+                GameObject.DontDestroyOnLoad(NewScreen);
+                return NewScreen;
+            }
+            return null;
+        }
+    }
+}
---
+++ CaptainsComms/Patch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CaptainsComms
+{
+    [HarmonyPatch(typeof(PLIntrepidInfo), "SetupShipStats")]
+    internal class IntrepidPatch
+    {
+        private static void Postfix(PLIntrepidInfo __instance, bool previewStats)
+        {
+            if (previewStats || !Mod.Config.Intrepid) return;
+            GameObject Screen = UsefulMethods.DuplicateCommsScreen(__instance);
+            if (Screen == null) return;
+            Screen.transform.localPosition = new Vector3(-2.2f, 0.4f, 12.5f);
+        }
+    }
+    [HarmonyPatch(typeof(PLOutriderInfo), "SetupShipStats")]
+    internal class OutriderPatch
+    {
+        private static void Postfix(PLIntrepidInfo __instance, bool previewStats)
+        {
+            if (previewStats || !Mod.Config.Outrider) return;
+            GameObject Screen = UsefulMethods.DuplicateCommsScreen(__instance);
+            if (Screen == null) return;
+            Screen.transform.localPosition = new Vector3(-1.6f, 2.15f, 2.5f);
+            Screen.transform.rotation = Quaternion.Euler(359.9f, 135.64f, 0f);
+        }
+    }
+    [HarmonyPatch(typeof(PLOldWarsShip_Sylvassi), "SetupShipStats")]
+    internal class SwordshipPatch
+    {
+        private static void Postfix(PLIntrepidInfo __instance, bool previewStats)
+        {
+            if (previewStats || !Mod.Config.Swordship) return;
+            GameObject Screen = UsefulMethods.DuplicateCommsScreen(__instance);
+            if (Screen == null) return;
+            Screen.transform.localPosition = new Vector3(2.17f, 4.78f, 14.08f);
+            Screen.transform.rotation = Quaternion.Euler(325.738f, 297.0586f, 0);
+        }
+    }
+}
